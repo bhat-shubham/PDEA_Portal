12e5b6db0fd391bff4c5606e980232c371127b08
Shubham Bhat <bhatsupshubham@gmail.com>
2025-09-01 14:44:19 +0530

    no bg issue for sm devices

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import ImageGallery from "@/components/ui/image-gallery";
 import LightRays from "@/components/ui/lightraysbg";
 export default function Home() {
   return (
-    <main className="relative w-full h-screen overflow-hidden">
-      <div className="absolute inset-0 z-0">
+    <main className="relative  w-full h-screen overflow-hidden">
+      <div className="hidden md:block absolute inset-0 z-0">
         <LightRays
           raysOrigin="top-center"
           raysColor="#00ffff"
           raysSpeed={1.5}
-          lightSpread={2.5}
-          rayLength={2}
+          lightSpread={3}
+          rayLength={3.5}
           followMouse={true}
           mouseInfluence={0.2}
           noiseAmount={0.1}
@@ -18,8 +18,8 @@ export default function Home() {
         />
       </div>
 
-      <div className="absolute inset-0 z-10 flex md:p-0 p-5 items-center justify-center">
-        <div className="md:w-3/4 md:p-5 w-full h-4/5 flex rounded-3xl bg-[#699fa20d] backdrop-blur-md overflow-hidden">
+      <div className="bg-[#2C2C54] md:bg-transparent absolute inset-0 z-10 flex md:p-0 p-5 items-center justify-center">
+        <div className="md:w-3/4 md:p-5 w-full md:h-4/5 h-3/5 flex rounded-3xl bg-[#699fa20d] backdrop-blur-md overflow-hidden">
           <div className="w-1/2 hidden md:flex overflow-hidden rounded-3xl items-center justify-center h-full">
             <ImageGallery/>
           </div>
